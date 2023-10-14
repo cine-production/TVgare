3cfbd48c950d089d528b8012f4b4ebecfb551fdc
cineprod <142513195+cine-production@users.noreply.github.com>
2023-10-14 20:13:12 +0200

    Votre message de commit ici

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import logo from './my1fursonas.png';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import React from 'react'
+import Home from './page/Home'
+import City from './page/City'
+import TrainStation from './components/TrainStation'
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} />
-      <h1>salut ! au revoirrrrrrr</h1> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path=":city" element={<City />}>
+          <Route path=":codeStation" element={<TrainStation />} />
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
