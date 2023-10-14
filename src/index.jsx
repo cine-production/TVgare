747ff315563707b368bd79060d71c9d91b48e40e
cineprod <142513195+cine-production@users.noreply.github.com>
2023-10-14 17:58:37 +0200

    Votre message de commit ici

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import createRoot from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './styles/index.scss'
+
+createRoot.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
