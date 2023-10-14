5a384712aa1861ad6573cc2af9e025287ac9e7c4
cineprod <142513195+cine-production@users.noreply.github.com>
2023-10-14 17:41:51 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './my1fursona.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} />
+    </div>
+  );
+}
+
+export default App;
